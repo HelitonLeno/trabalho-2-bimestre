a1ce18c59430739a8a0504ea40e7f68a84800c56
Heliton Leno <ciencia_@live.com>
2017-06-17 14:15:54 -0300

    Update Exercicio25.java

--- Exercicio25/src/Exercicio25.java
+++ Exercicio25/src/Exercicio25.java
@@ -1,19 +1,28 @@
 import java.util.Scanner;
 
 public class Exercicio25 {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
 
-        System.out.println("Qual numero de Pell deseja?: ");
+        System.out.println("Quantos numeros de Pell deseja gerar?: ");
         int numero = in.nextInt();
 
-        System.out.println(gerarNumeroDePell(numero));
+        imprimirSequenciaPell(numero, 0);
+    }
+
+    private static void imprimirSequenciaPell(int numero, int count) {
+        if (numero > count) {
+            System.out.printf("Pell %d -> %d \n", count,gerarNumeroDePell(count));
+
+            imprimirSequenciaPell(numero, ++count);
+        }
     }
 
     static int gerarNumeroDePell(int numero) {
         if (numero == 0 || numero == 1)
             return numero;
         else
-            return 2 * gerarNumeroDePell(numero - 1) + gerarNumeroDePell(numero -2);
+            return 2 * gerarNumeroDePell(numero - 1) + gerarNumeroDePell(numero - 2);
+
     }
 }
